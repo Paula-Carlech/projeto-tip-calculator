6d76448eff4f5edcfec3187a38b9ab9a74953d56
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2022-07-06 16:39:19 -0300

    New Updates

---
+++ tipCalculator.js
@@ -0,0 +1,27 @@
+function multi05(){
+    let bill = document.getElementById('bill')
+    let people = document.getElementById('people')
+    let result = (bill*people)*0.5
+    let res = document.querySelector('p#valor1')
+    res.innerHTML = `$ ${result}`
+}
+
+function multi10(){
+
+}
+
+function multi15(){
+
+}
+
+function multi25(){
+
+}
+
+function multi50(){
+
+}
+
+function custom(){
+
+}
